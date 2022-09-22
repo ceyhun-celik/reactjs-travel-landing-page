7ca8f62316a68e0b60cc8e73e89d3690e554ed0a
ceyhun-celik <ceyhuniverse@gmail.com>
2022-09-22 23:42:18 +0300

    search

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -46,7 +46,35 @@ const Search = () => {
             </div>
         </div>
 
-        <div className="">right</div>
+        <div>
+            <div className='border text-center'>
+                <p className='pt-2'>GET AN ADDITIONAL 10% OFF</p>
+                <p className='py-4'>12 HOURS LEFT</p>
+                <p className='bg-gray-800 text-gray-200 py-2'>BOOK NOW AND SAVE</p>
+            </div>
+
+            <form className='w-full'>
+                <div className='flex flex-col my-2'>
+                    <label>Destination</label>
+                    <select className='border rounded-md p-2'>
+                        <option>Grande Antigua</option>
+                        <option>Key West</option>
+                        <option>Maldives</option>
+                        <option>Cozumel</option>
+                    </select>
+                </div>
+                <div className='flex flex-col my-4'>
+                    <label>Check-In</label>
+                    <input className='border rounded-md p-2' type="date" />
+                </div>
+                <div className='flex flex-col my-2'>
+                    <label>Check-Out</label>
+                    <input className='border rounded-md p-2' type="date" />
+                </div>
+
+                <button className='w-full my-4'>Rates & Availabilities</button>
+            </form>
+        </div>
     </div>
   )
 }
